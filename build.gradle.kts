f0691da3ffe741b75c636f5455312a68b082ed32
yooni <hongsy0113@gmail.com>
2023-06-10 16:15:27 +0900

    :wrench: JsonConfiguration 추가
    - snake_case로 인식하도록 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
 	runtimeOnly("com.h2database:h2")
 	runtimeOnly("com.mysql:mysql-connector-j")
 
---
+++ src/main/kotlin/com/yooni/newsletter/configuration/JsonConfiguration.kt
@@ -0,0 +1,20 @@
+package com.yooni.newsletter.configuration
+
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class JsonConfiguration {
+
+    @Bean
+    fun objectMapper(): ObjectMapper =
+        jacksonObjectMapper()
+            .setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE)
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+            .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+}
